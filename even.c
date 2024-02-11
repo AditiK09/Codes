f888058c545620b8de32f8c82718ac67e6aa07c5
AditiKarra_ <148749846+AditiK09@users.noreply.github.com>
2024-02-11 19:47:21 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{  int n;
+printf("enter ur value:");
+scanf("%d",n);
+  
+if(n%2==0)
+    printf("the nu is even");
+    else{
+        printf("the no is not even");
+    }
+  
+  return 0;
+}
