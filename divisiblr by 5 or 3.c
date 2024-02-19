63f6e6c56ae0c7583df015af216c269eb2f18800
AditiKarra_ <148749846+AditiK09@users.noreply.github.com>
2024-02-19 23:24:12 +0530

    Add files via upload

---
+++ divisiblr by 5 or 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the value:");
+	scanf("%d",&n);
+	if(n%5==0 || n%3==0)
+{
+	printf("n is divisible by 5 or 3");}
+		else
+{
+	printf("n is not divisible by 5 or 3");	}
+	
+}
